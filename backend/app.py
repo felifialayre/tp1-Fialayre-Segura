af1936dedec7b8ecc3941214779582e31d1f75a6
Felipe <felipefialayre@gmail.com>
2024-07-16 16:15:41 -0300

    implementacion de la edicion/eliminacion del jugador con sus respectivos endpoints y frontend

--- backend/app.py
+++ backend/app.py
@@ -37,17 +37,16 @@ def hello():
 @app.route('/home')
 def return_players():
     contenido = []
-    count_jugadores = db.session.query(func.count(Jugador.id)).scalar()
-    for i in range (1, count_jugadores+1):
-        current = db.session.query(Jugador).filter_by(id=i).first()
-        current_dic = {
-            'id' : current.id,
-            'nombre' : current.nombre,
-            'avatar' : current.avatar,
-            'apodo' : current.apodo
+    jugadores = Jugador.query.all()
+    for jugador in jugadores:
+        jugador_dic = {
+            'id': jugador.id,
+            'nombre': jugador.nombre,
+            'avatar': jugador.avatar,
+            'apodo': jugador.apodo
         }
-        contenido.append(current_dic)
-    return contenido
+        contenido.append(jugador_dic)
+    return jsonify(contenido)
 
 @app.route('/players/<player_id>')
 def return_player_by_id(player_id):
@@ -57,10 +56,58 @@ def return_player_by_id(player_id):
         'nombre' : jugador.nombre,
         'avatar' : jugador.avatar,
         'edad' : jugador.edad,
-        'ganadas' : jugador.edad,
+        'ganadas' : jugador.ganadas,
         'perdidas' : jugador.perdidas,
         'apodo' : jugador.apodo,
     }
+
+@app.route('/players/<player_id>', methods = ["DELETE"])
+def delete_character_by_id(player_id):
+    jugador = db.session.query(Jugador).filter_by(id=player_id).first()
+    if jugador is not None:
+       db.session.delete(jugador)
+       db.session.commit()
+       return jsonify({"message": True}), 200
+    else:
+       print(f"No se encontró ningún jugador con ID {player_id}.")
+       return jsonify({"message": False}), 500
+    
+    
+@app.route('/edit/<player_id>', methods = ["PUT"]) 
+def edit_character_by_id(player_id):
+    try:
+        
+        data = request.get_json()
+        
+        if not data:
+            return jsonify({"error": "El cuerpo de la solicitud debe ser JSON"}), 400
+        nombre = data.get('nombre')
+        apodo = data.get('apodo')
+        edad = data.get('edad')
+        avatar = data.get('avatar')
+            
+        jugador = Jugador.query.get(player_id)
+        
+        if not jugador:
+            return jsonify({"error": "Jugador no encontrado"}), 404
+           
+        if nombre is not None:
+            jugador.nombre = nombre
+        if apodo is not None:
+            jugador.apodo = apodo
+        if edad is not None:
+            jugador.edad = edad
+        if avatar is not None:
+            jugador.avatar = avatar
+
+        db.session.commit()
+        
+        return jsonify({"mensaje" : True})
+
+    except Exception as e:
+        logging.error(f"Error creating player: {e}")
+        return jsonify({'message': False}), 500
+
 
 if __name__ == '__main__':
     logging.debug('Starting server...')
